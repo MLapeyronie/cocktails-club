9fa60b89f7858831ac3ef5de4a4eb6afc11fee88
Mathilde Lapeyronie <mathilde.lapeyronie@gmail.com>
2021-03-25 01:33:27 +0100

    css and back

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.destroy_all
+Cocktail.destroy_all
+
+# Ingredient.create(name: 'vodka')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'triple sec')
+# Ingredient.create(name: 'citron jaune')
+# Ingredient.create(name: 'cranberry')
+# Ingredient.create(name: 'whisky')
+# Ingredient.create(name: 'martini rouge')
+# Ingredient.create(name: 'angostura')
+# Ingredient.create(name: 'tequila')
+# Ingredient.create(name: 'citron vert')
+# Ingredient.create(name: 'martini dry')
+# Ingredient.create(name: 'gin')
+# Ingredient.create(name: 'olive')
+# Ingredient.create(name: 'saumure')
+# Ingredient.create(name: 'ecorce citron')
+# Ingredient.create(name: 'rhum')
+# Ingredient.create(name: 'feuilles de menthe')
+# Ingredient.create(name: 'eau gazeuse')
+# Ingredient.create(name: 'sucre')
+
+
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Cuba Libre')
+Cocktail.create(name: 'Magarita')
+Cocktail.create(name: 'Pina Colada')
+Cocktail.create(name: 'Martini')
